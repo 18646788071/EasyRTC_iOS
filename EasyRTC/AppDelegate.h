8aacfd00afcffccf79b4d606c26151d1fc4b0240
linyuliang <linyuliang@jd.com>
2017-10-10 11:40:17 +0800

    Initial Commit

---
+++ EasyRTC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EasyRTC
+//
+//  Created by David on 2017/10/10.
+//  Copyright © 2017年 David. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EasyRTC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EasyRTC
+//
+//  Created by David on 2017/10/10.
+//  Copyright © 2017年 David. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
